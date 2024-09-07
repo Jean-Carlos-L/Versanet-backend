822ff10784605c2c4bee9f66fafd0492bb62dea6
Jean-Carlos-L <yancalerma@gmail.com>
2024-09-07 10:13:01 -0500

    creation of project Basic structure of projec like architerure, deployment of first api

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to the API",
+  });
+});
+
+export default app;
