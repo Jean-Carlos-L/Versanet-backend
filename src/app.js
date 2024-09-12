3473ec531c10a0ec9b301f91de6cd0b4bc6ac455
patgeo117 <gpatino017@gmail.com>
2024-09-11 21:23:03 -0500

    Endpoints-Consultas-Mikrotik

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import { MikrotikService } from "./modules/mikrotik/services/mikrotik.service.js";
 
 const app = express();
 
@@ -14,9 +15,51 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.get("/", async (req, res) => {
+  
   res.json({
     message: "Welcome to the API",
   });
+});
+
+// Nuevo endpoint para obtener la identidad del sistema
+app.get("/system-identity", async (req, res) => {
+  try {
+    const identity = await MikrotikService.getSystemIdentity();
+    
+    // Devolver el valor al cliente
+    res.json(identity);
+  } catch (error) {
+    console.error("Error in /system-identity endpoint: ", error);
+    res.status(500).json({
+      error: "Failed to retrieve system identity",
+    });
+  }
+});
+
+app.get("/interface-traffic", async (req, res) => {
+  try {
+    const traffic = await MikrotikService.getInterfaceTraffic();
+    
+    res.json(traffic);
+  } catch (error) {
+    console.error("Error in /interface-traffic endpoint: ", error);
+    res.status(500).json({
+      error: "Failed to retrieve interface traffic",
+    });
+  }
+});
+
+app.get("/traffic-by-ip", async (req, res) => {
+  try {
+    const traffic = await MikrotikService.getTrafficByIP();
+    
+    res.json(traffic);
+  } catch (error) {
+    console.error("Error in /traffic-by-ip endpoint: ", error);
+    res.status(500).json({
+      error: "Failed to retrieve traffic by IP",
+    });
+  }
 });
 
 export default app;
--- src/common/config/mikrotik.connection.js
+++ src/common/config/mikrotik.connection.js
@@ -6,20 +6,25 @@ import {
   MIKROTIK_USER,
 } from "../constants/constants.js";
 
-export const connection = new Promise((resolve, reject) => {
-  const conn = new RouterOSAPI({
-    host: MIKROTIK_HOST,
-    port: MIKROTIK_PORT,
-    user: MIKROTIK_USER,
-    password: MIKROTIK_PASSWORD,
-  });
-  conn
-    .connect(() => {
-      console.log("Connection successfully established with Mikrotik");
-      resolve(conn);
-    })
-    .catch((err) => {
-      console.error("Error while trying to connect to Mikrotik", err);
-      reject(err);
+export const connection = () =>
+  new Promise((resolve, reject) => {
+    const conn = new RouterOSAPI({
+      host: MIKROTIK_HOST,
+      port: MIKROTIK_PORT,
+      user: MIKROTIK_USER,
+      password: MIKROTIK_PASSWORD,
+      timeout: 30000, // Ajusta el timeout según sea necesario
     });
-});
+
+    // Conexión correcta a la API usando promesas
+    conn
+      .connect()
+      .then(() => {
+        console.log("Connection successfully established with Mikrotik");
+        resolve(conn);
+      })
+      .catch((err) => {
+        console.error("Error while trying to connect to Mikrotik", err);
+        reject(err);
+      });
+  });
