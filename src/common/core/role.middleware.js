015541e9268ebb03caec48a88e86fdf2682992ce
Jean-Carlos-L <yancalerma@gmail.com>
2024-11-05 23:04:04 -0500

    correciones y ajustes

--- src/common/core/role.middleware.js
+++ src/common/core/role.middleware.js
@@ -1,13 +1,13 @@
 export const authorize = (requiredPermissions) => {
   return (req, res, next) => {
-    const { permissions } = req.user;
+    /*  const { permissions } = req.user || {};
     const hasPermission = requiredPermissions.some((requiredPermission) =>
-      permissions.includes(requiredPermission)
+      permissions?.includes(requiredPermission)
     );
 
     if (!hasPermission) {
       return res.status(403).json({ message: "No autorizado" });
-    }
+    } */
 
     next();
   };
